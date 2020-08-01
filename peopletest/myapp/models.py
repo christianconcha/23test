cf2f15774bd7d29f2aaa258d648917d1a1a96c41
Christian Concha <christianconchaflores@gmail.com>
2020-08-01 15:10:28 -0400

    - creating models according to json structure - Creating views - Adding url paths to views - Fixing issues with CSRF

--- peopletest/myapp/models.py
+++ peopletest/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	national_id   = models.CharField(max_length=20, null=True, blank=True)
+	name          = models.CharField(max_length=50, null=True,blank=True)
+	last_name     = models.CharField(max_length=50, null=True,blank=True)
+	age           = models.IntegerField()
+	origin_planet = models.CharField(max_length=99, null=True, blank=True) 
+	picture_url   = models.CharField(max_length=2084, null=True, blank=True)
+
+
--- peopletest/myapp/views.py
+++ peopletest/myapp/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# from django.views.decorators.csrf import csrf_exempt
+# from django.contrib.auth.decorators import login_required
+
+from myapp.models import Person
+
+import json
 
 # Create your views here.
+def index(request):
+	response = json.dumps([{}])
+	return HttpResponse(response, content_type='text/json')
+
+# @csrf_exempt
+def addPerson(request):
+	if request.method == 'POST':
+		#Gets json payload from request
+		json_payload = json.loads(request.body)
+		#Captures every variable inside json payload
+		national_id   = json_payload['national_id']
+		name          = json_payload['name']
+		last_name     = json_payload['last_name']
+		age           = json_payload['age']
+		origin_planet = json_payload['origin_planet']
+		picture_url   = json_payload['picture_url']
+			
+		#Create an object with new attributes
+		person        = Person(national_id=national_id, name=name, last_name=last_name, age=age, 
+								origin_planet=origin_planet, picture_url=picture_url)
+			
+		try:
+			#attempt to save object to DB
+			person.save()
+			#Confirm object saved
+			response = json.dumps([{'Success':'Status 201'}])
+		#if any error raises when saving to DB..
+		except:
+			response = json.dumps([{'Error':'Status 500'}])				
+
+	return HttpResponse(response, content_type='text/json')
+
+
+
+
+def getPeople(request, national_id):
+	if request.method == 'GET':
+		try:
+			#Try to get the person using national_id as key
+			human    = Person.objects.get(national_id= national_id)
+			response = json.dumps([{'national_id':human.national_id, 'name':human.name, 
+									'last_name':human.last_name , 'age':human.age, 
+									'origin_planet':human.origin_planet, 'picture_url':human.picture_url}]) 
+		except:
+			response = json.dumps([{'Error':'Status 404'}])
+
+	return HttpResponse(response, content_type='text/json')
+
+
