5706516ebe7061ddc5869ab35a42e4490a4336f9
Christian Concha <christianconchaflores@gmail.com>
2020-08-03 22:06:35 -0400

    Adding description to the base url

--- peopletest/myapp/views.py
+++ peopletest/myapp/views.py
@@ -14,8 +14,13 @@ logger = logging.getLogger('views.py')
 # Create your views here.
 
 def index(request):
-	# Index view, displays an empty json:
-	response = json.dumps([{}])
+	# Index view. It displays all path available :
+	response = json.dumps([{'Creates a person based on a json':'/post/people', 
+							'Retrieves a person by their national_id':'/get/people/"national_id"',
+							'Retrieves all data in DB':'/get/people',
+							'Updates a person data by their national_id':'/put/people/"national_id"',
+							'Deletes a person by national_id':'/delete/people/"national_id"'
+							}])
 	return HttpResponse(response, status= 200, content_type='application/json')
 
 
@@ -27,7 +32,7 @@ def addPerson(request):
 	if request.method == 'POST' and request.content_type == 'application/json':
 		# Gets json payload from request
 		json_payload = json.loads(request.body)
-		#Captures every variable inside json payload
+		#Captures every variable contained in json payload
 		national_id   = json_payload['national_id']
 		name          = json_payload['name']
 		last_name     = json_payload['last_name']
@@ -44,13 +49,16 @@ def addPerson(request):
 			person.save()
 			#Confirms object saved
 			response = json.dumps(json_payload)
+			# Setup http status code
 			status   = 201
 		#If any error raises when saving to DB..
 		except:
 			response = json.dumps([{'Error':'Status 500 INTERNAL SERVER ERROR'}])
+			# Setup http status code
 			status   = 500
 	else:
 		response 	 = json.dumps([{'Error':'Status 400 BAD REQUEST'}])
+		# Setup http status code
 		status 		 = 400
 	
 	return HttpResponse(response, status=status, content_type='application/json')
@@ -68,10 +76,12 @@ def getPersonById(request, national_id):
 			response  = json.dumps([{'national_id':person.national_id, 'name':person.name, 
 									'last_name':person.last_name , 'age':person.age, 
 									'origin_planet':person.origin_planet, 'picture_url':person.picture_url}]) 
+			# Setup http status code
 			status    = 200
 		#If any exception raises when saving to DB..
 		except:
 			response  = json.dumps([{'Error':'Status 404 NOT FOUND'}])
+			# Setup http status code
 			status 	  = 404
 
 	return HttpResponse(response, status=status, content_type='application/json')
@@ -84,14 +94,16 @@ def getAllPeople(request):
 
 	if request.method=='GET':
 		try:
-			#Gets everyones records 
+			#Gets everyone's records 
 			all_people    = Person.objects.all()
-			#Creates a json file with all records
+			#Serializes a json with all records
 			response 	  = serializers.serialize('json',all_people) 
+			# Setup http status code
 			status 		  = 200
 		#If any error raises when saving to DB..
 		except:
 			response      = json.dumps([{'Error':'Status 500 INTERNAL SERVER ERROR'}])
+			# Setup http status code
 			status 	  	  = 500			
 	
 	return HttpResponse(response, status=status, content_type='application/json')	
@@ -107,7 +119,7 @@ def updatePerson(request, national_id):
 	if request.method=='PUT' and request.content_type == 'application/json':
 		# Gets json payload from request
 		json_payload  = json.loads(request.body)
-		#Captures every variable inside Json payload
+		#Captures every variable contained in Json payload
 		nat_id        = json_payload['national_id']
 		name          = json_payload['name']
 		last_name     = json_payload['last_name']
@@ -123,20 +135,23 @@ def updatePerson(request, national_id):
 														     	  name=name, last_name=last_name , age=age, 
 														     	  origin_planet=origin_planet, picture_url=picture_url)
 			response 	 = json.dumps(json_payload)
+			# Setup http status code
 			status   	 = 200
 
 		except Person.DoesNotExist:
 			#If person doesn't exist:
 			response 	 = json.dumps([{'Error':'Status 404 NOT FOUND'}])
+			# Setup http status code
 			status   	 = 404
-			print("hola")
 		except:
 			# On any other errors set status to 500:
 			if not response:
 				response = json.dumps([{'Error':'Status 500 INTERNAL SERVER ERROR'}])
+				# Setup http status code
 				status   = 500
 	else:
 		response 		 = json.dumps([{'Error':'Status 400 BAD REQUEST'}])
+		# Setup http status code
 		status 		 	 = 400
 
 	return HttpResponse(response, status=status, content_type='application/json')	
@@ -146,23 +161,27 @@ def updatePerson(request, national_id):
 
 def deletePerson(request, national_id):
 	logger.debug("deletePerson")
+
 	response = []
 	if request.method=='DELETE':
 		try:
 			# Looks for a person based on national_id 
 			person   = Person.objects.get(national_id=national_id)
-			# Updates a person based on national_id and a json
+			# Deletes a person based on national_id and a json
 			person.delete()
 			response = json.dumps([{'Success':'Status 200 OK'}])
+			# Setup http status code
 			status   = 200
 		except Person.DoesNotExist:
 			#If person doesn't exist:
 			response = json.dumps([{'Error':'Status 404 NOT FOUND'}])
+			# Setup http status code
 			status   = 404
 		except:
 			# On any other errors set status to 500:
 			if not response:
 				response = json.dumps([{'Error':'Status 500 INTERNAL SERVER ERROR'}])
+				# Setup http status code
 				status   = 500
 
 	return HttpResponse(response, status=status, content_type='application/json')	
